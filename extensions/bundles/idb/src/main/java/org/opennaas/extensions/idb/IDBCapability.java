42ee42e64980f7043db295ccf23913c4b98552e8
santiago garcia pimentel <santiagozky@gmail.com>
2012-07-03 17:18:44 +0200

    deleted webservice control. web services now are managed by the capability

--- extensions/bundles/idb/src/main/java/org/opennaas/extensions/idb/IDBCapability.java
+++ extensions/bundles/idb/src/main/java/org/opennaas/extensions/idb/IDBCapability.java
@@ -1,5 +1,8 @@
 package org.opennaas.extensions.idb;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.ActivatorException;
@@ -9,8 +12,13 @@ import org.opennaas.core.resources.capability.AbstractCapability;
 import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.core.resources.descriptor.ResourceDescriptorConstants;
-import org.opennaas.extensions.idb.webservice.WebserviceControl;
+import org.opennaas.extensions.idb.serviceinterface.databinding.jaxb.NetworkNotificationPortType;
+import org.opennaas.extensions.idb.serviceinterface.databinding.jaxb.NetworkReservationPortType;
+import org.opennaas.extensions.idb.serviceinterface.databinding.jaxb.TopologyIFPortType;
+import org.opennaas.extensions.idb.webservice.ContextListener;
+import org.opennaas.extensions.idb.webservice.TopologyWS;
 import org.opennaas.extensions.queuemanager.IQueueManagerCapability;
+import org.osgi.framework.ServiceRegistration;
 
 public class IDBCapability extends AbstractCapability implements IIDBCapability {
 
@@ -20,9 +28,23 @@ public class IDBCapability extends AbstractCapability implements IIDBCapability 
 
 	private String resourceId = "";
 
-	public IDBCapability(CapabilityDescriptor descriptor, String resourceId) {
+	int port;
+	ServiceRegistration topologyRegistration;
+	ServiceRegistration notificationRegistration;
+	ServiceRegistration reservationRegistration;
+	ContextListener contextListener;
 
+	public IDBCapability(CapabilityDescriptor descriptor, String resourceId) {
 		super(descriptor);
+		port = Integer.parseInt(this.getCapabilityDescriptor()
+				.getProperty("port").toString());
+		if (port < 1024) {
+			log.equals("an IDB with a wrong port of: " + port
+					+ " could not be created");
+			throw new IllegalArgumentException(
+					"the descriptor contains an invalid port or under 1024");
+		}
+
 		this.resourceId = resourceId;
 		log.debug("Built new IDB Capability");
 	}
@@ -71,17 +93,70 @@ public class IDBCapability extends AbstractCapability implements IIDBCapability 
 
 	@Override
 	public void StartWebServices() {
-		int port = Integer.parseInt(this.getCapabilityDescriptor()
-				.getProperty("port").toString());
-		WebserviceControl.startWebservices(port);
-
+		contextListener();
+		startTopology();
+		startNotification();
+		startReservation();
 	}
 
 	@Override
 	public void StopWebServices() {
-		int port = Integer.parseInt(this.getCapabilityDescriptor()
-				.getProperty("port").toString());
-		WebserviceControl.stopWebServices(port);
+		contextListener.contextDestroyed();
+		topologyRegistration.unregister();
+		notificationRegistration.unregister();
+		reservationRegistration.unregister();
 
 	}
+
+	private void startTopology() {
+
+		Dictionary props = new Hashtable();
+		props.put("service.exported.interfaces", "*");
+		props.put("service.exported.configs", "org.apache.cxf.ws");
+		props.put("org.apache.cxf.ws.address", "http://localhost:" + port
+				+ "/opennaas/" + "/nsp/webservice/topology");
+		String[] features = { "org.apache.cxf.ws.addressing.WSAddressingFeature" };
+		props.put("org.apache.cxf.ws.features", features);
+		topologyRegistration = Activator.getContext().registerService(
+				TopologyIFPortType.class.getName(), new TopologyWS(), props);
+
+	}
+
+	private void startNotification() {
+		Dictionary props = new Hashtable();
+		props.put("service.exported.interfaces", "*");
+		props.put("service.exported.configs", "org.apache.cxf.ws");
+		props.put("org.apache.cxf.ws.address", "http://localhost:" + port
+				+ "/opennaas/" + "/nsp/webservice/notification");
+		String[] features = { "org.apache.cxf.ws.addressing.WSAddressingFeature" };
+		props.put("org.apache.cxf.ws.features", features);
+		notificationRegistration = Activator.getContext().registerService(
+				NetworkNotificationPortType.class.getName(), new TopologyWS(),
+				props);
+
+	}
+
+	private void startReservation() {
+
+		Dictionary props = new Hashtable();
+		props.put("service.exported.interfaces", "*");
+		props.put("service.exported.configs", "org.apache.cxf.ws");
+		props.put("org.apache.cxf.ws.address", "http://localhost:" + port
+				+ "/opennaas/" + "/nsp/webservice/reservation");
+		String[] features = { "org.apache.cxf.ws.addressing.WSAddressingFeature" };
+		props.put("org.apache.cxf.ws.features", features);
+		reservationRegistration = Activator.getContext().registerService(
+				NetworkReservationPortType.class.getName(), new TopologyWS(),
+				props);
+	}
+
+	private void contextListener() {
+		contextListener = new ContextListener();
+		contextListener.contextInitialized(); // this used to be a
+		// servletcontext listener. now
+		// we call before starting the
+		// ws
+
+	}
+
 }
